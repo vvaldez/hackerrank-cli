956698dd1595801d9d1ef64bc1106dde12da7b11
Vincent Valdez <vvaldez@Vincents-MacBook-Pro.local>
2015-06-02 19:22:12 -0500

    Add detail to README, fix  quesiton output

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # hackerrank-cli
-CLI for managing tests, questions and scripts for HackerRank API
+CLI for managing tests, questions and scripts for HackerRank API. This tool will query the HackerRank test or question URL as needed using a specified API key. With --get action, it will pull all question and scripts and write them to disk. With --put action it will POST the scripts from disk to the API.
+
+## Known Issues
+Currently POST does not work to the API and is being investigated.
+
+## Config file
+Some settings can be saved in .hackerrank-cli such as api_key and test_id in the format:
+
+.hackerrank-cli
+[Defaults]
+api_key = 1234567890
+test_id = 1234
+
+## Usage:
+incents-MacBook-Pro:hackerrank-cli vvaldez$ ./hackerrank-cli.py -h
+Usage:
+hackerrank-cli.py [options]
+
+This script will get and put tests questions and scripts via the HackerRank API.
+
+Use -h or --help for all available options.
+
+Options:
+  -h, --help            show this help message and exit
+  -g, --get             Get data
+  -p, --put             Put data NOT COMPLETE
+  -T, --tests           All Tests
+  -Q, --questions       All Questions
+  -t TEST_ID, --test=TEST_ID
+                        Test ID
+  -q QUESTION_ID, --question=QUESTION_ID
+                        Question ID
+  -k API_KEY, --api-key=API_KEY
+                        API Key
+  -v, --verbose         Enable verbose output
+  -d, --debug           Enable lots of debug output (more than verbose)
+
+## Examples
+* Get all tests:
+./hackerrank-cli.py --get --tests
+
+* Get all questions for a test
+./hackerrank-cli.py --get --test 1234
+
+NOTE: Feature not complete!
+* Post all questions for a test 
+./hackerrank-cli.py --put --test 1234
+
+## To Do:
+* Understand why POST isn't working (it even fails with a curl command)
+* Include question and test text to a file for editing and posting
+* Needs a lot of cleanup as this was very hackish
--- hackerrank-cli.py
+++ hackerrank-cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 # Import modules
@@ -9,12 +11,22 @@ except:
     print "Could not import modules"
     raise
 
+__author__ = "Vinny Valdez"
+__copyright__ = "Copyright 2015"
+#__credits__ = [""]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Vinny Valdez"
+__email__ = "vvaldez@redhat.com"
+__status__ = "Prototype"
+
 # config parsing
 config = ConfigParser.RawConfigParser()
 config.read('.hackerrank-cli')
 api_key = config.get('Defaults', 'api_key')
 test_id = config.get('Defaults', 'test_id')
 
+# HackerRank specific data
 tests_url = 'https://www.hackerrank.com/x/api/v2/tests'
 tests_url_params = {
 	'limit': '-1', 
@@ -24,9 +36,7 @@ question_url = 'https://www.hackerrank.com/x/api/v1/questions/'
 question_url_params = {
 	'access_token': api_key
 	}
-
 url_headers = {'content-type': 'application/json'}
-
 scripts = ['setup','check','solve','cleanup']
 
 # options parsing
@@ -35,7 +45,7 @@ description = "Use -h or --help for all available options."
 parser = OptionParser(usage=usage, description=description)
 #parser.add_option("-i", "--interactive", action="store_true", dest="interactive", help="Interactive Mode", default=False)
 parser.add_option("-g", "--get", action="store_true", dest="get", help="Get data", default=False)
-parser.add_option("-p", "--put", action="store_true", dest="put", help="Put data", default=False)
+parser.add_option("-p", "--put", action="store_true", dest="put", help="Put data **FEATURE NOT COMPLETE**", default=False)
 parser.add_option("-T", "--tests", action="store_true", dest="tests", help="All Tests", default=False)
 parser.add_option("-Q", "--questions", action="store_true", dest="questions", help="All Questions", default=False)
 parser.add_option("-t", "--test", dest="test_id", help="Test ID", default=test_id)
@@ -71,6 +81,8 @@ def get_data(url, parameters):
     return r.json()
 
 def put_data(url, params, payload):
+    if options.verbose:
+        print "INFO: url: %s \nheaders: %s \nparams: %s \ndata: %s" % (url, url_headers, params, json.dumps(payload))
     r = requests.post(url, headers=url_headers, params=params, data=json.dumps(payload))
     return r
 
@@ -142,30 +154,29 @@ def main(args):
                 print "options %s " % options.test_id
                 print "INFO: Test %s requested." % options.test_id
             questions = get_questions(options.test_id)
-            print "Questions: "
-            print "--------------+------------------------------------"
-            print "| Question ID | Name "
-            print "--------------+------------------------------------"
-            for question in questions:
-                    print "%s | %s " % (question, options.test_id)
-            print "--------------+------------------------------------"
             # Get test data to retrieve test name
             tests = get_tests(tests_url, tests_url_params)['data']
             for test in tests:
                     if str(test['id']) == str(options.test_id):
                             test_name = test['name']
             # To retrieve all scripts for a question
-            if options.questions:
+            if options.questions or options.test_id:
+                print "Questions: "
+                print "--------------+------------------------------------"
+                print "| Question ID | Name "
+                print "--------------+------------------------------------"
                 for question_id in questions:
                     if options.verbose:
                             print "Retrieving scripts for: %s" % question_id
                     question = get_question(question_id)['model']
                     q_name = question['name']
+                    print "%s | %s " % (question_id, q_name)
                     for script in scripts:
                             write_q_to_disk(test_name, options.test_id, q_name, question_id, script, question['sudorank_scripts'][script])
                     write_q_to_disk(test_name, options.test_id, q_name, question_id, "test_id", str(options.test_id))
                     write_q_to_disk(test_name, options.test_id, q_name, question_id, "question_id", question_id)
-            elif options.question:
+                print "--------------+------------------------------------"
+            elif options.question_id:
                 if options.verbose: 
                         print "INFO: Question %s requested." % question_id
                 questions = get_questions(options.test_id)
@@ -173,9 +184,9 @@ def main(args):
                 write_q_to_disk(test_name, options.test_id, q_name, question_id, "test_id", str(options.test_id))
                 write_q_to_disk(test_name, options.test_id, q_name, question_id, "question_id", question_id)
         # To retrieve a single question
-        elif options.question and options.test_id:
+        elif options.question_id and options.test_id:
                 if options.verbose: 
-                        print "INFO: Test %s Question %s requested." % (options.test_id, options.question)
+                        print "INFO: Test %s Question %s requested." % (options.test_id, options.question_id)
                 questions = get_questions(options.test_id)
     elif options.put:
         if options.verbose: 
